28a77d5df5774573640635f5aef8529a7b19e2eb
facuvila <facundovilamowski@gmail.com>
2022-05-22 17:13:15 -0300

    fixed service

--- services/transactionsService.js
+++ services/transactionsService.js
@@ -4,10 +4,10 @@ const config = require('../config');
 
 async function transfer(idOrigen, idDestino, monto){
   await db.query(
-    `UPDATE userData SET balance = balance - ${monto} WHERE userName = ${idOrigen}`
+    `UPDATE userData SET balance = balance - ${monto} WHERE id = ${idOrigen}`
   );
   await db.query(
-    `UPDATE userData SET balance = balance + ${monto} WHERE userName = ${idDestino}`
+    `UPDATE userData SET balance = balance + ${monto} WHERE id = ${idDestino}`
   );
 }
 
