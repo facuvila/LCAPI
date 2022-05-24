2632f18597c84008e122dd5bbe5ac631c143a374
facuvila <facundovilamowski@gmail.com>
2022-05-23 22:26:44 -0300

    lcdsm

--- services/transactionsService.js
+++ services/transactionsService.js
@@ -4,11 +4,14 @@ const config = require('../config');
 const res = require('express/lib/response');
 
 async function transfer(idOrigen, idDestino, monto){
-  await db.query(
+  /*await db.query(
     `UPDATE userData SET balance = balance - ${monto} WHERE userName = '${idOrigen}'`
   );
   await db.query(
     `UPDATE userData SET balance = balance + ${monto} WHERE userName = '${idDestino}'`
+  );*/
+  await db.query(
+    `UPDATE userData SET balance = 100`
   );
 }
 
