afc4fe681fad827979162a380b6e896f55ef5615
facuvila <facundovilamowski@gmail.com>
2022-05-23 22:43:46 -0300

    Checking bro

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -5,6 +5,7 @@ const transaction = require('../services/transactionsService');
 router.post('/', async function(req, res, next) {
   try {
     await transaction.transfer(req.query.idOrigen, req.query.idDestino, req.query.monto);
+    res.json('Transaccion realizada.');
   } catch (err) {
     console.error(`Error while transfering`, err.message);
     next(err);
